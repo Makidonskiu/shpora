1c8ddcaf13e2799f9cda702e896831691a625a3c
Makidonskiu <www.salamandr@mail.ru>
2022-07-28 22:19:09 +0300

    mobile v

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo = () => {
   return (
-    <div>
+    <div className='logo'>
       <img width={'100%'} src="https://trello.com/1/cards/61a906cc29a9ce61408bae2a/attachments/61a906cc29a9ce61408baf05/previews/61a906cc29a9ce61408baf0d/download/photo_2021-09-08_10.48.41.jpeg" alt="logo" />
     </div>
   );
